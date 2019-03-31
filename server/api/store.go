889585e3e4d517eab999b41cfb44161a80fbd58c
sitRyo <al15030@shibaura-it.ac.jp>
2019-03-31 12:02:24 +0900

    add webAPI and routing

---
+++ server/api/store.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StoreTweet(c *gin.Context) {
+	fmt.Print("hi")
+}
---
+++ server/router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func router(e *gin.Engine) {
+	// api := e.Group("/api")
+
+	// block
+	{
+		// /api/upload/
+		// e.POST("/upload", api.StoreTweet)
+	}
+}
