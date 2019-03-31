1c2fe273c4f13a206c848c2623fe8fcd55a929b8
sitRyo <al15030@shibaura-it.ac.jp>
2019-03-31 11:18:46 +0900

    create front and server directory

---
+++ front/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Front/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Front from './components/Front.vue';
+
+@Component({
+  components: {
+    Front,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 30px;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ front/src/components/Front.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <div class="tweet-wrapper">
+      <div class="header">つぶやき</div>
+      <div class="body">
+        <textarea v-model="tweet" class="text-area"></textarea>
+        <button class="tweet-button">ツイートする</button>
+      </div>
+    </div>
+    <!-- 5件ほど表示させる。Apiを自動で叩く？ -->
+    <div class="tweet-histroy"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Front extends Vue {
+  tweet: string = '';
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.tweet-wrapper {
+  float: left;
+}
+
+</style>
+
